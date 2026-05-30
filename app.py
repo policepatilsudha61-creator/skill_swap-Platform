70f5150777549ed7b5f5089edff5f7525ceff825
Sudha <LENOVO@DESKTOP-1SEAERQ>
2026-05-30 11:43:44 +0530

    fix mysql

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = "skillswap123"
+# db = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="sudha@123",
+#     database="skill_swap"
+# )
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="sudha@123",
-    database="skill_swap"
-)
+# cursor = db.cursor()
 
-cursor = db.cursor()
 
 @app.route('/')
 def home():
